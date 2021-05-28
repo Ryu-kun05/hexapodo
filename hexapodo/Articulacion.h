5873ec429e74cc6e9e47d81c69ba923511709194
¨gg¨ <gg@alzago.com>
2021-05-28 21:04:36 +0200

    Creando el metodo Articulacion::mueveGrado

--- hexapodo/Articulacion.h
+++ hexapodo/Articulacion.h
@@ -1,6 +1,9 @@
 #ifndef __ARTICULACION_H__
 #define __ARTICULACION_H__
 
+#include <Servo.h> 
+
+#define GRADO_INICIO 90
 
 class Articulacion {
   private:
@@ -8,8 +11,10 @@ class Articulacion {
     int pin;
 
   public:
-  
-  
+    Articulacion(const int pinArg); 
+    bool mueveGrado(const int gradoDestino);
+                                                                                        
+                                    
 };
 
 #endif
--- hexapodo/hexapodo.ino
+++ hexapodo/hexapodo.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+#include"Articulacion.h"
 
+void setup() {
+  Serial.begin(9600);
+  Articulacion a1(9);
+
+  a1.mueveGrado(180);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  
 
 }
